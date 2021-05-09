97302af121b956c667154fa4b9e5d5d936f21430
PawelKobus <pawelkobus920@gmail.com>
2021-05-09 16:45:55 +0200

    0.34 added salary

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Date;
+
 public class Human {
     String firstName;
     String lastName;
@@ -9,4 +11,29 @@ public class Human {
     Phone mobilePhone;
     Animal pet;
     Car audi;
+
+    private Double salary;
+
+    public Human(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getSalary() {
+        System.out.println("pobrana wartosc wynagrodzenia " + this.salary);
+        System.out.println("termin pobrania informacji:" + new Date());
+        return salary;
+    }
+    public void setSalary( double salary){
+        if(salary < 0.0){
+            System.out.println("sr, nie za free");
+        }
+        else{
+            System.out.println("nowe dane zostały wysłane do systemu księgowego");
+            System.out.println("odebrać aneks do umowy od pani Hani z kadr");
+            System.out.println("ZUS i US już wiedzą o zmianie wypłaty i nie ma sensu ukrywać dochodu");
+        }
+
+    }
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,29 @@ public class Main {
 
         Animal dog = new Animal("Canis familiaris");
         dog.name = "Szarik";
-        Car audi = new Car("Audi","A3");
-        Phone iphone = new Phone("apple","S6","iOS",4.5);
+        Car audi = new Car("Audi", "A3");
+        Phone iphone = new Phone("apple", "S6", "iOS", 4.5);
 
         System.out.println("producer = " + iphone.producer);
         System.out.println("name = " + dog.name);
 
-        Human me = new Human();
-        me.mobilePhone = iphone;
-        dog.feed();
+        Human me = new Human(1000.0);
+        Double mySalary = me.getSalary();
+        me.setSalary(2000.0);
+
+
+
+        /*dog.feed();
         dog.takeForAWalk();
         dog.feed();
         dog.takeForAWalk();
         dog.takeForAWalk();
         dog.feed();
         dog.takeForAWalk();
-        dog.takeForAWalk();
+        dog.takeForAWalk();*/
+
+
+
+
     }
 }
